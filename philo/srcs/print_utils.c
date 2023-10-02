1ffbe0e823cbe3049daaf40b12a53840aa6e5dbf
Diego Tome Perez <dtome-pe@cbr6s3.42barcelona.com>
2023-10-02 09:51:41 +0200

    change printf type

--- philo/srcs/print_utils.c
+++ philo/srcs/print_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   print_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: theonewhoknew <theonewhoknew@student.42    +#+  +:+       +#+        */
+/*   By: dtome-pe <dtome-pe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 10:12:59 by theonewhokn       #+#    #+#             */
-/*   Updated: 2023/09/30 10:42:32 by theonewhokn      ###   ########.fr       */
+/*   Updated: 2023/10/02 09:47:31 by dtome-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	print_params(t_param *param)
 
 static void	print_aux(int64_t ms, int id, char *str)
 {
-	printf("%lu %d %s\n", ms, id, str);
+	printf("%llu %d %s\n", ms, id, str);
 }
 
 void	print_state(t_philo *philo, int type)
